a5b6bf929703bfca5c101e0eb8e1ebc14372513b
joeldevel <christianjoel.developer@gmail.com>
2021-03-26 18:36:28 -0300

    Basic algorithm working. Thanks to geeksforgeeks.org!

--- convert-to-roman.js
+++ convert-to-roman.js
@@ -1,7 +1,24 @@
 function convertToRoman(num) {
-  // get how many digit num has
-  const length = num.toString().length;
-  return num;
+  // all possible bases
+  let numArray = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+  let romanSymbols = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+  let i = 12;
+  let div;
+  let result = ""
+    while(num>0)
+    {
+      // integer division
+      div = Math.floor(num/numArray[i])
+      num = num%numArray[i];
+      while(div)
+      {
+        console.log(romanSymbols[i]);
+        result+=romanSymbols[i];
+        div--;
+      }
+      i--;
+    }
+    return result;
 }
 
-convertToRoman(302);
+console.log(convertToRoman(3549));
